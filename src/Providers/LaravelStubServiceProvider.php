9c7c95c837540b261c09958b6ca014cf63f8efdd
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2024-03-03 16:46:17 +0330

    Update LaravelStubServiceProvider.php

--- src/Providers/LaravelStubServiceProvider.php
+++ src/Providers/LaravelStubServiceProvider.php
@@ -2,9 +2,16 @@
 
 namespace Binafy\LaravelStub\Providers;
 
+use Binafy\LaravelStub\LaravelStub;
 use Illuminate\Support\ServiceProvider;
 
 class LaravelStubServiceProvider extends ServiceProvider
 {
-
+    /**
+     * Boot providers.
+     */
+    public function boot(): void
+    {
+        $this->app->bind('laravel-stub', fn ($app) => new LaravelStub);
+    }
 }
