1158dc6704e8d71ea6f16031e68ddf9a7ebf2321
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2024-03-03 16:49:43 +0330

    Update LaravelStub.php

--- src/LaravelStub.php
+++ src/LaravelStub.php
@@ -4,5 +4,20 @@ namespace Binafy\LaravelStub;
 
 class LaravelStub
 {
+    /**
+     * Stub path.
+     *
+     * @var string
+     */
+    protected string $from;
 
+    /**
+     * Get stub path.
+     */
+    public function from(string $path): static
+    {
+        $this->from = $path;
+
+        return $this;
+    }
 }
